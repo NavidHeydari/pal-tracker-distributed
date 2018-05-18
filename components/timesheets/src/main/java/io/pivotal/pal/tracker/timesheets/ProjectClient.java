51d28078fa2ebf65ba842dede5f00dc753aba399
Navid <navid.heidary@gmail.com>
2018-05-18 14:54:54 -0600

    completed hystrix lab - circut breaker

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,18 +1,41 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String endpoint;
+    Map<Long, ProjectInfo> inMemoryCached = new ConcurrentHashMap<>();
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
         this.endpoint = registrationServerEndpoint;
     }
-
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+         ProjectInfo projectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+
+         //saved into in-memory cache
+        logger.info("SAVING project INFO into CACHE as well with id {}.", projectId);
+        inMemoryCached.put(projectId, projectInfo);
+
+        return projectInfo;
     }
+
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        logger.info("Getting project with id {} from cache", projectId);
+        return inMemoryCached.get(projectId);
+    }
+
 }
